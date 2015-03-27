0d656dee282fd3006c3ff2eb2ca9a5fead97ca07
Yuxin (David) Huang <dhuang@colgate.edu>
2015-03-27 11:23:50 -0400

    passed 17 tests, 13 more to go

--- myrouter.py
+++ myrouter.py
@@ -25,10 +25,10 @@ class Router(object):
 		'''
 		self.net = net
 		self.interfaces = self.net.interfaces()
-		self.arpcache = {}
-		self.arpqueue = queue.Queue()
 		self.fwdtable = {}
 		self.fill_fwdtable()
+		self.arpcache = {}
+		self.arpqueue = queue.Queue()
 		
 	def fill_fwdtable(self):
 		'''
@@ -43,13 +43,13 @@ class Router(object):
 		The table is keyed by each interface's name.
 		'''
 		for intf in self.interfaces: # initially the table contains interfaces only
-			# set nexthop for our own interfaces to null (no need to forward)
-			prefixnet = IPv4Network(str(intf.ipaddr) + "/" + str(intf.netmask))
+			# set nexthop for directly connected network to null (no need to forward)
+			prefixnet = IPv4Network(str(intf.ipaddr) + "/" + str(intf.netmask), strict=False)
 			self.fwdtable[intf.name] = [(prefixnet, IPv4Address(0))]
 
 		forwarding_table = open('forwarding_table.txt')
 		for line in forwarding_table:
-			temp = line.split(' ')
+			temp = line.strip('\n').split(' ')
 			prefixnet = IPv4Network(temp[0] + "/" + temp[1])
 			nexthop = IPv4Address(temp[2])
 			intf = temp[3]
@@ -60,13 +60,6 @@ class Router(object):
 		 		self.fwdtable[intf] = [(prefixnet, nexthop)]
 
 		forwarding_table.close()
-
-	def got_reply(self, arp):
-		'''
-		(Arp) -> (bool)
-		'''
-		for interface in self.
-
 
 	def fwdtable_lookup(self, ipv4):
 		'''
@@ -79,52 +72,75 @@ class Router(object):
 		specifies which item to look for in the list of network prefixes
 		associated with the interface in question.
 		'''
-		dst_ipaddr = IPv4Address(ipv4.dst)
-		possible_prefixes = []
+		dst_ipaddr = IPv4Address(ipv4.dstip)
+		most_precise_prefix = (None, None, -1, None)
 		for intf in self.fwdtable.keys():				# every interface in forwarding table
 			for i in range(len(self.fwdtable[intf])):	# every prefix asc.'d w/ the interface
 				prefixnet, nexthop = self.fwdtable[intf][i]
-				if str(nexthop) == '0.0.0.0':	# one of our interfaces, dropping
-					return None, -2
-				if dst_ipaddr in prefix:
-					possible_prefixes += [(prefixnet, intf, i)]
-
-		if len(possible_prefixes) == 0:
-			return None, -1
-
-		# look for most precise prefix
-		most_precise_prefix = possible_prefixes[0][0]
-		for prefix in possible_prefixes:
-			curr_prefixlen = prefix[0].prefixlen
-			if most_precise_prefix.prefixlen < curr_prefixlen:
-				most_precise_prefix = prefix
+				if dst_ipaddr in prefixnet:
+					if most_precise_prefix[0] == None:
+						most_precise_prefix = (prefixnet, intf, i, nexthop)
+					elif most_precise_prefix[0].prefixlen < prefixnet.prefixlen:
+						most_precise_prefix = (prefixnet, intf, i, nexthop)
 
 		return most_precise_prefix[1:]
 
-	def ready_packet(self, intf, ind, pkt):
+	def ready_packet(self, intf_name, ind, pkt, nexthop):
 		'''
 		Precondition: pkt has IPv4 header
 		'''
+		intf = self.net.interface_by_name(intf_name)
+
 		ipv4 = pkt[1]
 		ipv4.ttl -= 1
 
 		eth = pkt[0]
 		eth.src = intf.ethaddr
 
+		if str(nexthop) == "0.0.0.0":	# directly connected network
+			nexthop = ipv4.dstip
+
 		# check ARP cache
-		if ipv4.dstip in self.arpcache:
-			eth.dst = self.arpcache[ipv4.dstip]
-			self.net.send_packet(intf, pkt)
+		if nexthop in self.arpcache:
+			eth.dst = self.arpcache[nexthop]
+			self.net.send_packet(intf.name, pkt)
 		else:	# create ARP request
-			arppacket = create_arp_req(intf, ipv4.dstip)
-			self.net.send_packet(intf, arppacket)
+			arppacket = self.create_arp_req(intf, nexthop)
+			self.net.send_packet(intf.name, arppacket)
 			senttime = time.time()
-			queue_pkt = ARPQueuePacket(pkt)
+
+			queue_pkt = ARPQueuePacket(pkt, intf)
 			queue_pkt.update_rqst_time(senttime)
+			queue_pkt.retries += 1
+			queue_pkt.nexthop = nexthop
 			self.arpqueue.put(queue_pkt)
 
 	def send_enqueued_packets(self):
-
+		for i in range(self.arpqueue.qsize()):
+			curr_pkt = self.arpqueue.get()
+			# check cache
+			if curr_pkt.nexthop not in self.arpcache:
+				# need to check time of last ARP request
+				if time.time() - curr_pkt.last_request > 1:
+					# ARP again
+					curr_pkt.retries += 1
+					if not curr_pkt.is_dead():
+						arppacket = self.create_arp_req(curr_pkt.interface_tosend,
+							curr_pkt.nexthop)
+						self.net.send_packet(curr_pkt.interface_tosend.name, arppacket)
+						senttime = time.time()
+						curr_pkt.update_rqst_time(senttime)
+						self.arpqueue.put(curr_pkt)
+					else:
+						log_debug('Too many ARP requests, dropping packet: {}'
+							.format(str(curr_pkt.packet)))
+				else:	# put it back if it hasn't been a second
+					self.arpqueue.put(curr_pkt)
+			else:	# got ARP reply
+				eth = curr_pkt.packet[0]
+				eth.dst = self.arpcache[curr_pkt.nexthop]
+				self.net.send_packet(curr_pkt.interface_tosend.name, 
+					curr_pkt.packet)
 
 	def create_arp_req(self, intf, targetip):
 		ether = Ethernet()
@@ -132,12 +148,12 @@ class Router(object):
 		ether.dst = 'ff:ff:ff:ff:ff:ff'
 		ether.ethertype = EtherType.ARP
 		arp_req = Arp()
-		arp.operation = ArpOperation.Request
-		arp.senderhwaddr = intf.ethaddr
-		arp.senderprotoaddr = intf.ipaddr
-		arp.targethwaddr = 'ff:ff:ff:ff:ff:ff'
-		arp.targetprotoaddr = targetip
-		arppacket = ether + arp
+		arp_req.operation = ArpOperation.Request
+		arp_req.senderhwaddr = intf.ethaddr
+		arp_req.senderprotoaddr = intf.ipaddr
+		arp_req.targethwaddr = 'ff:ff:ff:ff:ff:ff'
+		arp_req.targetprotoaddr = targetip
+		arppacket = ether + arp_req
 
 		return arppacket
 
@@ -152,7 +168,7 @@ class Router(object):
 		while True:
 			gotpkt = True
 			try:
-				dev, pkt = self.net.recv_packet(timeout=1.0)
+				dev_name, pkt = self.net.recv_packet(timeout=1.0)
 			except NoPackets:
 				log_debug("No packets available in recv_packet")
 				gotpkt = False
@@ -160,28 +176,33 @@ class Router(object):
 				log_debug("Got shutdown signal")
 				break
 
+			self.send_enqueued_packets()
 			if gotpkt:
 				log_info("Got a packet: {}".format(str(pkt)))
 				
 				arp = pkt.get_header(Arp)
 				ipv4 = pkt.get_header(IPv4)
-				if arp != None:		# has ARP header
-					if arp.targetprotoaddr == dev.ipaddr
+				dev = self.net.interface_by_name(dev_name)
+				if arp is not None:		# has ARP header
+					if arp.targetprotoaddr == dev.ipaddr:
 						if arp.targethwaddr == 'ff:ff:ff:ff:ff:ff':	# need to reply
 							arp_reply = create_ip_arp_reply(dev.ethaddr,
 															arp.senderhwaddr,
 															dev.ipaddr,
 															arp.senderprotoaddr)
-							self.net.send_packet(dev, arp_reply)
+							self.net.send_packet(dev_name, arp_reply)
+							self.send_enqueued_packets()
 						else: # got reply
 							self.arpcache[arp.senderprotoaddr] = arp.senderhwaddr
 							self.send_enqueued_packets()
 					else:
 						log_info("ARP request not for us, dropping: {}".format(str(pkt)))
-				elif ipv4 != None:	# has IPv4 header
-					intf, index = self.fwdtable_lookup(ipv4)
-					if index != -1:	# has a match
-						self.ready_packet(intf, index, pkt)
+				elif ipv4 is not None:	# has IPv4 header
+					intf, index, nexthop = self.fwdtable_lookup(ipv4)
+					if index != -1 and index != -2:	# has a match
+						# debugger()
+						self.ready_packet(intf, index, pkt, nexthop)
+						self.send_enqueued_packets()
 					elif index == -1:	# no match
 						log_info("No match in fwding table, dropping: {}".format(str(pkt)))
 					else:	# index == -2, our packet; drop it for now
@@ -190,15 +211,17 @@ class Router(object):
 					log_info("Got packet that is not ARP or IPv4, dropping: {}".format(str(pkt)))
 
 class ARPQueuePacket(object):
-	def __init__(self, pkt):
-		self.pkt = pkt
+	def __init__(self, pkt, intf):
+		self.packet = pkt
 		self.retries = 0
 		self.last_request = time.time()
+		self.interface_tosend = intf
+		self.nexthop = IPv4Address(0)
 
 	def is_dead(self):
 		return self.retries > 5
 
-	def update_rqst_time(self, time)
+	def update_rqst_time(self, time):
 		self.last_resend = time
 
 def switchy_main(net):
